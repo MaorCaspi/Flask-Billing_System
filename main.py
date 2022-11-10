aaae28320c92d578cbe6192500a701ea17dd5a0c
Maor <Maor@MAOR-PC>
2022-11-10 16:21:40 +0200

    Add apscheduler.schedulers

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import requests
+import requests, datetime
 from flask import Flask
 from flask_restful import Api, Resource, reqparse
 from urllib import request
+from apscheduler.schedulers.background import BackgroundScheduler
 
 PROCESSOR_URL = "http://127.0.0.1:5000"
 SRC_BANK_ACCOUNT = "424158"
@@ -9,14 +10,11 @@ SRC_BANK_ACCOUNT = "424158"
 app = Flask(__name__)
 api = Api(app)
 
-perform_advance_args = reqparse.RequestParser() # Validate body parames for perform advance
-perform_advance_args.add_argument("dst_bank_account", type=str, help="dst_bank_account is required", required=True)
-perform_advance_args.add_argument("amount", type=float, help="amount is required", required=True)
-
 def did_transaction_succeeded(transaction_id):
     '''
     The function recive transaction_id and check the transaction status.
-    : If the trunsaction status is success -> return True, else return False.
+
+    If the trunsaction status is success -> return True, else return False.
     '''
     try:
         with request.urlopen(PROCESSOR_URL) as f:
@@ -37,10 +35,11 @@ def did_transaction_succeeded(transaction_id):
             raise Exception("Problem with getting the report from the Processor server")
 
     
-def perform_debit(dst_bank_account, amount):
+def perform_debit(dst_bank_account, amount, end_repayment_plan_date):
     '''
     The function perform a debit to dst_bank_account by amounts.
-    : If the trunsaction status is success -> return True, else return False.
+
+    If the trunsaction status is success -> return True, else return False.
     '''
     parameters_to_processor = {
                                 "src_bank_account" : SRC_BANK_ACCOUNT,
@@ -54,22 +53,33 @@ def perform_debit(dst_bank_account, amount):
         if res.status_code == 200 :
             transaction_id = res.text[1:-2] # Remove the quotation marks and the new line and save it
             if not did_transaction_succeeded(transaction_id): # If the debit transaction failed
-                print("!!!!!!!!!!")###########################
+                scheduler.add_job(perform_debit, 'date', run_date=end_repayment_plan_date, args=[dst_bank_account, amount, end_repayment_plan_date], misfire_grace_time=None)
         else:
             raise Exception("There was an error on the Processor server while tring perform transaction: " + res.text)
     except:
             raise Exception("The Processor server is unavailable")
 
+perform_advance_args = reqparse.RequestParser() # Validate body parames for perform advance
+perform_advance_args.add_argument("dst_bank_account", type=str, help="dst_bank_account is required", required=True)
+perform_advance_args.add_argument("amount", type=float, help="amount is required", required=True)
+
+scheduler = BackgroundScheduler()
+scheduler.start()
 
 class Advance(Resource):
     def post(self):
         '''
         The system credits the customer with the amount.
-        :In the following 12 weeks, the system performs debits of amount/12 once a week.
-        :A failed debit is moved to the end of the repayment plan (a week from the last payment).
+
+        In the following 12 weeks, the system performs debits of amount/12 once a week.
+
+        A failed debit is moved to the end of the repayment plan (a week from the last payment).
         '''
         args = perform_advance_args.parse_args()
-        perform_debit(args["dst_bank_account"], args["amount"]) ################
+        end_repayment_plan_date = datetime.datetime.now() + datetime.timedelta(7*12,) # Run in the following 12 weeks
+        for i in range(12):
+            run_start_date = datetime.datetime.now() + datetime.timedelta(7*i,) # Run in the following 12 weeks
+            scheduler.add_job(perform_debit, 'date', run_date=run_start_date, args=[args["dst_bank_account"], args["amount"]/12, end_repayment_plan_date], misfire_grace_time=None)
 
         return("OK", 200)
 
