dfc69d8ac949151b2ecd1112b507949d066cc817
Maor <Maor@MAOR-PC>
2022-11-11 18:00:00 +0200

    Improves

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests, datetime
 from flask import Flask
-from flask_restful import Api, reqparse
+from flask_restful import reqparse
 from urllib import request
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
@@ -9,14 +9,12 @@ PROCESSOR_URL = "http://127.0.0.1:5000"
 SRC_BANK_ACCOUNT = "424158"
 
 app = Flask(__name__)
-api = Api(app)
 
 def did_transaction_succeeded(transaction_id):
-    '''
+    """
     The function recive transaction_id and check the transaction status.
-
     If the trunsaction status is success -> return True, else return False.
-    '''
+    """
     try:
         with request.urlopen(PROCESSOR_URL) as f:
             report_from_processor = f.read().decode('utf-8')
@@ -37,11 +35,10 @@ def did_transaction_succeeded(transaction_id):
 
     
 def perform_debit(dst_bank_account, amount, end_repayment_plan_date):
-    '''
+    """
     The function perform a debit to dst_bank_account by amounts.
-
     If the trunsaction status is success -> return True, else return False.
-    '''
+    """
     parameters_to_processor = {
                                 "src_bank_account" : SRC_BANK_ACCOUNT,
                                 "dst_bank_account" : dst_bank_account,
@@ -63,24 +60,22 @@ def perform_debit(dst_bank_account, amount, end_repayment_plan_date):
     except:
             raise Exception("The Processor server is unavailable")
 
-perform_advance_args = reqparse.RequestParser() # Validate body parames for perform advance
-perform_advance_args.add_argument("dst_bank_account", type=str, help="dst_bank_account is required", required=True)
-perform_advance_args.add_argument("amount", type=float, help="amount is required", required=True)
-
 scheduler = BackgroundScheduler(jobstores = {'default': SQLAlchemyJobStore(url='sqlite:///jobs.sqlite')}, job_defaults={'misfire_grace_time': None})
 scheduler.start()
 
 @app.route('/', methods=['POST'])
 def perform_advance():
-    '''
+    """
     The system credits the customer with the amount.
-
     In the following 12 weeks, the system performs debits of amount/12 once a week.
-
     A failed debit is moved to the end of the repayment plan (a week from the last payment).
-    '''
+    """
 
-    args = perform_advance_args.parse_args()
+    parser = reqparse.RequestParser() # Validate body parames for perform advance
+    parser.add_argument("dst_bank_account", type=str, help="dst_bank_account is required", required=True)
+    parser.add_argument("amount", type=float, help="amount is required", required=True)
+
+    args = parser.parse_args()
     end_repayment_plan_date = datetime.datetime.now() + datetime.timedelta(7*12,0) # Run in the following 12 weeks
     for i in range(12):
         run_start_date = datetime.datetime.now() + datetime.timedelta(7*i,0) # Run in the following 12 weeks
